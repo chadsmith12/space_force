f17868844923abf33676751272d3d7371b62d34d
Chad Smith <chadsmith4@my.unt.edu>
2018-08-21 00:35:55 -0500

    added a shredder that removes the lasers the player shoots when they get out of sight.

---
+++ Space Force/Assets/Scripts/Shredder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Player 
+{
+	public class Shredder : MonoBehaviour 
+	{
+		void OnTriggerEnter2D(Collider2D collider)
+		{
+			Destroy(collider.gameObject);
+		}
+	}
+}
+
