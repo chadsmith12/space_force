f5ce4e60be774ee0e750110fd22a575e8eec1b7d
Chad Smith <chadsmith4@my.unt.edu>
2018-08-21 20:02:32 -0500

    rearranging all the scripts

--- Space Force/Assets/Scripts/Player/Shredder.cs
+++ Space Force/Assets/Scripts/Player/Shredder.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 namespace Assets.Scripts.Player 
 {
+	// The shredder class is responsible for destroying the game objects if they go too far off screen
+	// Destroys the player projectiles as they keep going up.
 	public class Shredder : MonoBehaviour 
 	{
 		void OnTriggerEnter2D(Collider2D collider)
