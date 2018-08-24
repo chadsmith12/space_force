0b95bedf38923440b47c99d02290f10839c8f144
Chad Smith <chadsmith4@my.unt.edu>
2018-08-24 00:15:13 -0500

    Add a WaveConfig to configure waves. EnemyPathing now uses this wave config

---
+++ Space Force/Assets/Scripts/Enemy/WaveConfig.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+	[SerializeField] private GameObject _enemyPrefab;
+	[SerializeField] private GameObject _pathPrefab;
+	[SerializeField] private float _timeBetweenSpawns = 0.5f;
+	[SerializeField] private float _spawnRandomFactor = 0.3f;
+	[SerializeField] private int _numberOfEnemies = 5;
+	[SerializeField] private float _moveSpeed = 2f;
+
+	// Gets the prefab for this enemy.
+	public GameObject EnemyPrefab
+	{
+		get 
+		{
+			return _enemyPrefab;
+		}
+	}
+	// Gets the prefab for this path.
+	public IEnumerable<Transform> Waypoints
+	{
+		get 
+		{
+			var waveWayPoints = _pathPrefab.transform.OfType<Transform>().Select(child => child);
+			return waveWayPoints;
+		}
+	}
+	// Gets the time between enemy spawns.
+	public float TimeBetweenSpawns 
+	{
+		get 
+		{
+			return _timeBetweenSpawns;
+		}
+	}
+
+	// Gets the random factor of the spawning
+	public float SpawnRandomFactor 
+	{
+		get
+		{
+			return _spawnRandomFactor;
+		}
+	}
+
+	// Gets the number of enemies this wave will have in it
+	public int NumberOfEnemies
+	{
+		get
+		{
+			return _numberOfEnemies;
+		}
+	}
+
+	// Gets the movement speed of the wave.
+	public float MoveSpeed 
+	{
+		get
+		{
+			return _moveSpeed;
+		}
+	}
+}
