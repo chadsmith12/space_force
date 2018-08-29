160bf8d1360c2e66f983f58d76e2b2b46de4aee7
Chad Smith <chadsmith4@my.unt.edu>
2018-08-28 22:13:05 -0500

    added ability to deal and enemies take damage

---
+++ Space Force/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+	[SerializeField] private float _health = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		var damageDealer = other.gameObject.GetComponent<DamageDealer>();
+		_health -= damageDealer.Damage;
+	}
+}
