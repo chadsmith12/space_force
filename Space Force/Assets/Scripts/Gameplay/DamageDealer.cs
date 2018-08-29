2c9fa755b9b97cf345ecb0182ab9ef7cd8d66fc2
Chad Smith <chadsmith4@my.unt.edu>
2018-08-28 22:13:05 -0500

    added ability to deal and enemies take damage

---
+++ Space Force/Assets/Scripts/Gameplay/DamageDealer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour 
+{
+	[SerializeField] private int _damage = 100;
+
+	// Gets the amount of damage this does.
+	public int Damage 
+	{
+		get
+		{
+			return _damage;	
+		}
+	}
+
+	public void Hit()
+	{
+		Destroy(gameObject);
+	}
+}
