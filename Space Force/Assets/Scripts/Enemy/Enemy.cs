9ed528ea7d38692459cab08ed333df9d89ec3ead
Chad Smith <chadsmith4@my.unt.edu>
2018-08-29 23:54:29 -0500

    added ability for enemy to shoot back at the player

--- Space Force/Assets/Scripts/Enemy/Enemy.cs
+++ Space Force/Assets/Scripts/Enemy/Enemy.cs
@@ -7,12 +7,52 @@ namespace Enemy
 	public class Enemy : MonoBehaviour 
 	{
 		[SerializeField] private float _health = 100;
-		
+		[SerializeField] private float _shotCounter;
+		[SerializeField] private float _minTimeBetweenShots = 0.2f;
+		[SerializeField] private float _maxTimeBetweenShots = 3f;
+		[SerializeField] private GameObject _projectilePrefab;
+		[SerializeField] private float _projectileSpeed = 10f;
+
+		void Start() 
+		{
+			ResetShotCounter();
+		}
+
+		void Update()
+		{
+			CountDownAndShoot();
+		}
+
+		private void CountDownAndShoot() 
+		{
+			// take down the shot counter how long it was on the frame
+			// if the shot counter runs out, the enemy can fire.
+			_shotCounter -= Time.deltaTime;
+
+			if(_shotCounter <= 0.0f)
+			{
+				Fire();
+				ResetShotCounter();
+			}
+		}
+
+		private void ResetShotCounter() 
+		{
+			_shotCounter = Random.Range(_minTimeBetweenShots, _maxTimeBetweenShots);
+		}
+
+		private void Fire()
+		{
+			var projectile = Instantiate(_projectilePrefab, transform.position, Quaternion.identity);
+			projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, _projectileSpeed);
+		}
+
 		void OnTriggerEnter2D(Collider2D other)
 		{
 			var damageDealer = other.gameObject.GetComponent<DamageDealer>();
 			ProcessHit(damageDealer);
 		}
+
 		private void ProcessHit(DamageDealer damageDealer)
 		{
 			_health -= damageDealer.Damage;
