7275dddae64abf4abc6661fd70abfe5ed44579d4
Chad Smith <chadsmith4@my.unt.edu>
2018-09-01 23:30:08 -0500

    added a scrolling background

---
+++ Space Force/Assets/Scripts/Gameplay/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour 
+{
+	[SerializeField] private float _backgroundScrollSpeed = 0.5f;
+
+	private Material _textureMaterial;
+	private Vector2 _offset;
+
+	void Start() 
+	{
+		_textureMaterial = GetComponent<Renderer>().material;
+		_offset = new Vector2(0, _backgroundScrollSpeed);
+	}
+
+	void Update()
+	{
+		_textureMaterial.mainTextureOffset += _offset * Time.deltaTime;
+	}
+}
