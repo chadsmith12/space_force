9d704df4defe2ae5b262b5ef490ca564866b700e
Chad Smith <chadsmith4@my.unt.edu>
2018-11-23 14:05:12 -0600

    start to add main menu

--- Space Force/Assets/Scripts/Gameplay/BackgroundScroller.cs
+++ Space Force/Assets/Scripts/Gameplay/BackgroundScroller.cs
@@ -1,22 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BackgroundScroller : MonoBehaviour 
+namespace Assets.Scripts.Gameplay
 {
-	[SerializeField] private float _backgroundScrollSpeed = 0.5f;
+    public class BackgroundScroller : MonoBehaviour
+    {
+        [SerializeField] private float _backgroundScrollSpeed = 0.5f;
 
-	private Material _textureMaterial;
-	private Vector2 _offset;
+        private Material _textureMaterial;
+        private Vector2 _offset;
 
-	void Start() 
-	{
-		_textureMaterial = GetComponent<Renderer>().material;
-		_offset = new Vector2(0, _backgroundScrollSpeed);
-	}
+        void Start()
+        {
+            _textureMaterial = GetComponent<Renderer>().material;
+            _offset = new Vector2(0, _backgroundScrollSpeed);
+        }
 
-	void Update()
-	{
-		_textureMaterial.mainTextureOffset += _offset * Time.deltaTime;
-	}
+        void Update()
+        {
+            _textureMaterial.mainTextureOffset += _offset * Time.deltaTime;
+        }
+    }
 }
