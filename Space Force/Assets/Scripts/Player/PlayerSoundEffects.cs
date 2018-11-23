677380d5f9aa37941e5e402e52c6a3f2d664e992
Chad Smith <chadsmith4@my.unt.edu>
2018-11-23 11:26:23 -0600

    player and enemy sound effects

---
+++ Space Force/Assets/Scripts/Player/PlayerSoundEffects.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Gameplay;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    [CreateAssetMenu(menuName = "Player Sound Effects")]
+    public class PlayerSoundEffects : ScriptableObject
+    {
+        [SerializeField] private SoundEffectOptions _playerDeathSoundConfig;
+        [SerializeField] private SoundEffectOptions _playerShootSoundConfig;
+
+        public SoundEffectOptions DeathSound
+        {
+            get
+            {
+                return _playerDeathSoundConfig;
+            }
+        }
+
+        public SoundEffectOptions ShootSound
+        {
+            get
+            {
+                return _playerShootSoundConfig;
+            }
+        }
+    }
+}
