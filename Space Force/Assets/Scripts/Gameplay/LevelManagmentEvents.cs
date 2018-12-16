d56cdd27b82c00ba1ce16502c35a184f34cce9b0
Chad Smith <chadsmith4@my.unt.edu>
2018-12-16 10:24:18 -0600

    game over loads when player dies in the game

---
+++ Space Force/Assets/Scripts/Gameplay/LevelManagmentEvents.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Gameplay
+{
+    public class LevelManagmentEvents : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            LevelManagment.LoadGame();
+        }
+
+        public void QuitGame()
+        {
+            LevelManagment.QuitGame();
+        }
+    }
+}
