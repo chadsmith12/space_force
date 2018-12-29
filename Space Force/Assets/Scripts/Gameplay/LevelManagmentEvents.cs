bab2beff17493045ef0b8a716f4a65e69f5e273e
Chad Smith <chadsmith4@my.unt.edu>
2018-12-29 13:19:04 -0600

    add music the game that plays through all scenes

--- Space Force/Assets/Scripts/Gameplay/LevelManagmentEvents.cs
+++ Space Force/Assets/Scripts/Gameplay/LevelManagmentEvents.cs
@@ -4,6 +4,11 @@ namespace Assets.Scripts.Gameplay
 {
     public class LevelManagmentEvents : MonoBehaviour
     {
+        public void StartMenu()
+        {
+            LevelManagment.LoadStartMenu();
+        }
+
         public void StartGame()
         {
             LevelManagment.LoadGame();
---
+++ Space Force/Assets/Scripts/Gameplay/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Gameplay
+{
+    public class MusicPlayer : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            SetupSingleton();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void SetupSingleton()
+        {
+            if(FindObjectsOfType(GetType()).Length > 1)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
+
