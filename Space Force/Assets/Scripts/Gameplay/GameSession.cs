ea07659f648ac04afe523d4d82b421bd4e4abada
Chad Smith <chadsmith4@my.unt.edu>
2018-12-29 19:13:17 -0600

    Add game score and a game session singleton

---
+++ Space Force/Assets/Scripts/Gameplay/GameSession.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Gameplay
+{
+    public class GameSession : MonoBehaviour
+    {
+        /// <summary>
+        /// The current instance of this game session instance.
+        /// </summary>
+        public static GameSession Instance { get; private set; }
+
+        /// <summary>
+        /// The current score for this game session.
+        /// </summary>
+        public int CurrentScore { get; private set; }
+
+        void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        /// <summary>
+        /// Adds the score value to the current score of this game session.
+        /// </summary>
+        /// <param name="scoreValue">How much to add to the score.</param>
+        public void AddToScore(int scoreValue)
+        {
+            CurrentScore += scoreValue;
+        }
+
+        /// <summary>
+        /// Destroys this current game session to reset it.
+        /// </summary>
+        public void ResetGame()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Space Force/Assets/Scripts/Player/ScoreDisplayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Player
+{
+    public class ScoreDisplayer : MonoBehaviour
+    {
+        private Text _scoreText;
+
+        // Use this for initialization
+        void Start()
+        {
+            _scoreText = GetComponent<Text>(); 
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _scoreText.text = Gameplay.GameSession.Instance.CurrentScore.ToString();
+        }
+    }
+}
+
