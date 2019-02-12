0ba7e0ac74aef3b69aa47e5b03d57de9ef16c2d5
Chad Smith <chadsmith4@my.unt.edu>
2019-02-12 00:26:10 -0600

    spinning enemy bomb

---
+++ Space Force/Assets/Scripts/Gameplay/Spinner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Gameplay
+{
+    public class Spinner : MonoBehaviour
+    {
+        [SerializeField] float _speedOfSpin = 1f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(new Vector3(0, 0, _speedOfSpin * Time.deltaTime));
+        }
+    }
+}
